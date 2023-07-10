6ea7a2c9083a631cb42a36c9937c7675eb3b7db2
sugoiJG <jasongolliet@icloud.com>
2023-07-09 18:17:37 -0700

    main functionality of Home page added with picture and lorem ipsum

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar />
+    <Home />
     </>
   )
 }
